da4ed84731715e4eb9661fd17bab472c0720889c
Frida_Cera <fridaapodaca541@gmail.com>
2025-02-27 04:19:01 +0000

    enviado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,103 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MisIconos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconos extends StatelessWidget {
+  const MisIconos({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Frida Apodaca'),
+          titleTextStyle:
+              const TextStyle(color: Color(0xffb4c0e1), fontSize: 30),
+          centerTitle: true,
+          backgroundColor: const Color(0xff1d3857),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 30),
+              Text(
+                'frida sofia apodaca cera MAT:22308051281136',
+                style: TextStyle(fontSize: 20, color: Color(0xff103d52)),
+              ),
+              // Primera fila con 2 iconos
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  IconWithLabel(
+                    icon: Icons.star,
+                    label: 'Favorito',
+                    color: Colors.yellow,
+                  ),
+                  IconWithLabel(
+                    icon: Icons.favorite,
+                    label: 'favoritos',
+                    color: Colors.red,
+                  ),
+                  IconWithLabel(
+                    icon: Icons.search,
+                    label: 'Me gusta',
+                    color: Color(0xff000000),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20), // Espacio entre las filas
+              // Segunda fila con 1 icono
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  IconWithLabel(
+                    icon: Icons.thumb_up,
+                    label: 'me gusta',
+                    color: Colors.blue,
+                  ),
+                  IconWithLabel(
+                    icon: Icons.thumb_down_alt,
+                    label: 'no me gusta',
+                    color: Color(0xfff10808),
+                  ),
+                  IconWithLabel(
+                    icon: Icons.add,
+                    label: 'me gusta',
+                    color: Color(0xff8e21f3),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
+} // fin clase mis iconos
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class IconWithLabel extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final Color color;
+
+  const IconWithLabel({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.color,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50, color: color),
+        const SizedBox(height: 8), // Espacio entre el ícono y el texto
+        Text(label, style: const TextStyle(fontSize: 16)),
+      ],
     );
   }
 }
